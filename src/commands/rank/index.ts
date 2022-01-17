953f17c8978167aeec6c4ce0f7facbbed2178d83
Max <maxnatural13@gmail.com>
2022-01-17 15:11:42 -0600

    Added !rank command with Riot API

--- src/commands/rank/index.ts
+++ src/commands/rank/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { Actions, CommonUserstate } from "tmi.js";
+import config from "../../cfg/config";
 export = {
   name: "rank",
   aliases: [],
@@ -17,8 +18,16 @@ export = {
     var tagged = (context[0]) ? context[0] : user;
     tagged = (tagged?.startsWith("@")) ? tagged.substring(1) : tagged;
 
-    var query = axios({
-      url: "na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/esfand"
-    })
+    var query = await axios({
+      method: "GET",
+      url: "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/tVcXlTGiJle6Bjpy3n53bIR3PqNt9ksrfrwdLpmYpmjlozk?api_key=RGAPI-cbf8843c-8c94-4535-ae15-a5ebdcb70614",
+    });
+
+    var res = query.data[0];
+    var tier = res["tier"].toLowerCase();
+    var currentRank = `${tier.charAt(0).toUpperCase() + tier.slice(1)} ${res["rank"]}`;
+    var winLossRatio = (res["losses"]+res["wins"]) * 100;
+    var winLoss = `${res["wins"]}W ${res["losses"]}L %${winLossRatio.toString().slice(0, -2)} win ratio. OPGG: https://na.op.gg/summoner/userName=Esfand`;
+    client.action(channel, `@${tagged} current rank: ${currentRank} - ${winLoss}`);
   }
 }
