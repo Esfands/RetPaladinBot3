acb5714a961940c4f122eb6210f88482df0f734f
Max <maxnatural13@gmail.com>
2022-01-25 11:01:36 -0600

    Fixed stream stats so it updates on mongo

--- src/events/onHostingEvent/index.ts
+++ src/events/onHostingEvent/index.ts
@@ -1,6 +1,6 @@
 import { Actions } from "tmi.js";
-import { updateOne } from "../../utils/maria";
+import { StreamStat } from "../../schemas/StreamStatsSchema";
 
 export default async (client: Actions, channel: string, target: string, viewers: number) => {
-   await updateOne(`UPDATE streamstats SET Hosting='${channel}' WHERE Streamer='esfandtv';`);
+   StreamStat.updateOne({ type: "esfandtv" }, { hosting: channel });
 }
