3caa54c6f8564c1f64c3f5d13f236c40e813252a
Max <maxnatural13@gmail.com>
2022-01-26 01:23:00 -0600

    Changed !chatters to offline only

--- src/commands/chatters/index.ts
+++ src/commands/chatters/index.ts
@@ -8,12 +8,12 @@ const chattersCommand: CommandInt = {
   permissions: [],
   globalCooldown: 10,
   cooldown: 30,
-  description: "Check how many chatters are Esfand's channel.",
+  description: "Check how many offline chatters are Esfand's channel.",
   dynamicDescription: [
     "<code>!chatters</code>"
   ],
   testing: false,
-  offlineOnly: false,
+  offlineOnly: true,
   code: async (client: Actions, channel: string, userstate: CommonUserstate, context: Array<string>) => {
     const user = userstate["display-name"];
     let target = getTarget(user, context[0]);
