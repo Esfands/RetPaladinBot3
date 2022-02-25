a20791eeca1733acb3c12998162eabcdbf1d558f
Max <maxnatural13@gmail.com>
2022-02-25 16:17:57 -0600

    Fixed !followage bug to remove @ and ,

--- src/commands/followage/index.ts
+++ src/commands/followage/index.ts
@@ -1,5 +1,5 @@
 import { Actions, CommonUserstate } from "tmi.js";
-import { calcDate, getTarget } from "../../utils";
+import { calcDate, ErrorType, getTarget, logError } from "../../utils";
 import { isFollowingUser } from "../../utils/helix";
 import { CommandInt } from "../../validation/CommandSchema";
 const followage: CommandInt = {
@@ -23,22 +23,33 @@ const followage: CommandInt = {
     chTarget = (chTarget) ? chTarget : "esfandtv";
 
     if (!tagged) return;
-    let following = await isFollowingUser(chTarget, tagged.toLowerCase());
-    if (following === null) return client.action(channel, `@${user} "${tagged}" isn't a valid username.`);
-
-    if (following["data"].length !== 0) {
-      let foundDate = following["data"][0]["followed_at"];
-      let elapsed = calcDate(new Date(), new Date(foundDate), true);
-      if (tagged.toLowerCase() === userstate["username"]) {
-        if (chTarget.toLowerCase() === "esfandtv") {
-          client.action(channel, `@${tagged}, you have been following for ${elapsed}`);
-        } else return client.action(channel, `@${tagged}, you have been following ${chTarget} for ${elapsed}`);
-      } else {
-        if (chTarget.toLowerCase() === "esfandtv") {
-          client.action(channel, `${tagged} has been following for ${elapsed}`);
-        } else return client.action(channel, `${tagged} has been following ${chTarget} for ${elapsed}`);
-      }
-    } else client.action(channel, `@${user} ${tagged} doesn't follow ${chTarget}.`);
+    if (chTarget.includes(",")) chTarget = chTarget.replace(/,/g, '');
+    if (chTarget.includes("@")) chTarget = chTarget.replace(/@/g, '');
+  
+    if (tagged.includes(",")) tagged = tagged.replace(/,/g, '');
+    if (tagged.includes("@")) tagged = tagged.replace(/@/g, '');
+  
+    try {
+      let following = await isFollowingUser(chTarget, tagged);
+      if (following === null) return client.action(channel, `@${user} "${tagged}" isn't a valid username.`);
+  
+      if (following["data"].length !== 0) {
+        let foundDate = following["data"][0]["followed_at"];
+        let elapsed = calcDate(new Date(), new Date(foundDate), true);
+        if (tagged.toLowerCase() === userstate["username"]) {
+          if (chTarget.toLowerCase() === "esfandtv") {
+            client.action(channel, `@${tagged}, you have been following for ${elapsed}`);
+          } else return client.action(channel, `@${tagged}, you have been following ${chTarget} for ${elapsed}`);
+        } else {
+          if (chTarget.toLowerCase() === "esfandtv") {
+            client.action(channel, `${tagged} has been following for ${elapsed}`);
+          } else return client.action(channel, `${tagged} has been following ${chTarget} for ${elapsed}`);
+        }
+      } else client.action(channel, `@${user} ${tagged} doesn't follow ${chTarget}.`);
+    } catch(error) {
+      logError(user!, ErrorType.API, `Error fetching followage with target "${tagged}" in #${chTarget}`, new Date());
+      return client.action(channel, `@${user} FeelsDankMan sorry, there was an an issue getting the followage for user(s) ${tagged} in channel ${chTarget}.`);
+    }
   }
 }
 
--- src/commands/namechange/index.ts
+++ src/commands/namechange/index.ts
@@ -1,4 +1,5 @@
 import { Actions, CommonUserstate } from "tmi.js";
+import { StreamStat } from "../../schemas/StreamStatsSchema";
 import { CommandInt } from "../../validation/CommandSchema";
 
 const namechangeCommand: CommandInt = {
@@ -16,6 +17,11 @@ const namechangeCommand: CommandInt = {
   OnlineOnly: false,
   Code: async (client: Actions, channel: string, userstate: CommonUserstate, context: Array<string>) => {
     // change mongodb notifications for each notification type.
+    let liveNotification = await StreamStat.findOne({ type: "live" });
+    let allGameNotification = await StreamStat.findOne({ type: "game" });
+    let titleNotification = await StreamStat.findOne({ type: "title" });
+    let offlineNotification = await StreamStat.findOne({ type: "offline" });
+
     // change username and displayname in chatters table
     // change username in notifications table
   }
