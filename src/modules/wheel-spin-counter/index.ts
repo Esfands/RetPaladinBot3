104709d2e5a6b00e14bd31c6b6d90879bfc75ac1
Max <maxnatural13@gmail.com>
2022-02-27 13:59:12 -0600

    Added support for sub bombs for wheel spin

--- src/modules/wheel-spin-counter/index.ts
+++ src/modules/wheel-spin-counter/index.ts
@@ -4,13 +4,22 @@ export async function addSubToSpin(subs: number, gifter: string) {
   let query = await findQuery(`SELECT * FROM wheelspin`);
   let stats = query[0];
 
-  if (gifter.toLowerCase() !== "esfandtv") {
-    if (stats.Gifted >= stats.AmountNeeded) {
-      let overtime =  stats.Gifted - stats.AmountNeeded;
-      await updateOne(`UPDATE wheelspin SET Gifted=${overtime ? overtime : 0}, WheelSpins=${stats.WheelSpins+1}`);
-  
-    } else {
-      await updateOne(`UPDATE wheelspin SET Gifted=${stats.Gifted + subs}`);
+  if (stats.IsCollective === "true") {
+    if (gifter.toLowerCase() !== "esfandtv") {
+      if (stats.Gifted >= stats.AmountNeeded) {
+        let overtime =  stats.Gifted - stats.AmountNeeded;
+        await updateOne(`UPDATE wheelspin SET Gifted=${overtime ? overtime : 0}, WheelSpins=${stats.WheelSpins+1}`);
+    
+      } else {
+        await updateOne(`UPDATE wheelspin SET Gifted=${stats.Gifted + subs}`);
+      }
+    }
+  } else {
+    if (gifter.toLowerCase() !== "esfandtv") {
+      if (subs >= stats.AmountNeeded) {
+        let spinMath = subs/stats.AmountNeeded;
+        await updateOne(`UPDATE wheelspin SET WheelSpins=${stats.WheelSpins+spinMath}`);
+      }
     }
   }
 }
