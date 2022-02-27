95a8e97611710975fdd7bb066f0b5852c8b14559
Max <maxnatural13@gmail.com>
2022-02-26 22:38:58 -0600

    Added what wheelspin does

--- src/commands/wheelspin/index.ts
+++ src/commands/wheelspin/index.ts
@@ -20,8 +20,15 @@ const wheelspinCommand: CommandInt = {
     const user = userstate["display-name"];
     let target = getTarget(user, context[0]);
 
+    let one = Math.trunc(6/18 * 100) + "% - 10 gifted subs.";
+    let two = Math.trunc(5/18  * 100) + "% - 20 gifted subs.";
+    let three = Math.trunc(4/18 * 100) + "% - 40 gifted subs.";
+    let four = Math.trunc(2/18 * 100) + "% - power hour.";
+    let five = Math.trunc(1/18 * 100) + "% - 60 gifted subs.";
+
+    console.log(one, two, three, four, five)
     let wheelStats = await findQuery(`SELECT * FROM wheelspin`);
-    return client.action(channel, `@${target} to spin the wheel it requires a cumulative amount of ${wheelStats[0].AmountNeeded} subs. We're at ${wheelStats[0].Gifted} and Esfand owes ${wheelStats[0].WheelSpins} spins.`);
+    return client.action(channel, `@${target} to spin the wheel it requires a cumulative amount of ${wheelStats[0].AmountNeeded} subs. We're at ${wheelStats[0].Gifted} and Esfand owes ${wheelStats[0].WheelSpins} spins. ${one} ${two} ${three} ${four} ${five}`);
   }
 }
 
