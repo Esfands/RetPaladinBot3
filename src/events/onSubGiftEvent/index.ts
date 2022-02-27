fc26a9937f061e5c34cec2bbcbad13a4863217f2
Max <maxnatural13@gmail.com>
2022-02-27 01:55:03 -0600

    fixed subgift event

--- src/events/onSubGiftEvent/index.ts
+++ src/events/onSubGiftEvent/index.ts
@@ -3,9 +3,6 @@ import { addSubToSpin } from "../../modules/wheel-spin-counter"
 import { getBestAvailableEmote } from "../../utils/emotes";
 
 export default async (client: any, channel: any, username: any, streakMonths: any, recipient: any, methods: any, userstate: any) => {
-  const tierList: any = { 1000: 'Tier 1', 2000: 'Tier 2', 3000: 'Tier 3' };
-  const { prime, plan, planName } = methods;
-  client.say(channel, `@EsfandTV - ${username} has gifted a ${tierList[plan]} sub to ${recipient} ${getBestAvailableEmote(["PogU", "PagMan", "Pog", "PogChamp"])}`);
   subathonLogEvent(userstate, SubathonStatType.SUB, 1);
   addSubToSpin(1, username);
 }
