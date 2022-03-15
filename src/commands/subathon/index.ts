49d9ae49c61e5a99d25c0a74c1dd1d07b8e3eb28
Max <maxnatural13@gmail.com>
2022-03-15 11:23:28 -0500

    Reverted changes for subathon command

--- src/commands/subathon/index.ts
+++ src/commands/subathon/index.ts
@@ -24,8 +24,8 @@ const subathon: CommandInt = {
     let isPowerHour = query.IsPowerHour;
     let powerHour = (isPowerHour === "true") ? true : false;
     let res = `Subathon is now! ${(powerHour ? 60 : 30)} seconds per sub/$5/500 bits. Tier 2 is ${powerHour ? 60*2 : 30*2} seconds. Tier 3 is ${powerHour ? 60*5 : 30*5} seconds.`
-    //client.action(channel, `@${tagged} ${res}`);
-    client.action(channel, `@${tagged} Subathon is on pause while Esfand goes to L.A. for the streamer awards (March 12th @ 5pm PST). Timer is paused at 12h 9m 41s, donations and subs still add to the timer. Full details here: https://www.youtube.com/watch?v=uwgGPzYtoek`);
+    client.action(channel, `@${tagged} ${res}`);
+    //client.action(channel, `@${tagged} Subathon is on pause while Esfand goes to L.A. for the streamer awards (March 12th @ 5pm PST). Timer is paused at 12h 9m 41s, donations and subs still add to the timer. Full details here: https://www.youtube.com/watch?v=uwgGPzYtoek`);
   }
 }
 
