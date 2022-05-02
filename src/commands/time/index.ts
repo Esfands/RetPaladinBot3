3b2425aa76c95801fb2faf3d2f8e178995b72d28
Max <maxnatural13@gmail.com>
2022-05-02 16:06:01 -0500

    Added korean time to !time

--- src/commands/time/index.ts
+++ src/commands/time/index.ts
@@ -20,10 +20,20 @@ const time: CommandInt = {
     let tagged = getTarget(user, context[0]);
     let response = null;
 
+    function addHours(numOfHours: number, date = new Date()) {
+      date.setTime(date.getTime() + numOfHours * 60 * 60 * 1000);
+    
+      return date;
+    }
+
     let date = new Date();
     let cst = date.toLocaleString("en-US", { timeZone: "America/Chicago", hour: 'numeric', minute: 'numeric', hour12: true });
     let mil = date.toLocaleString("en-US", { timeZone: "America/Chicago", hour: 'numeric', minute: 'numeric', hour12: false });
-    response = `Esfand's local time is ${cst} CST KKona (${mil})`;
+
+    let date2 = addHours(9, date);
+    let kst = date2.toLocaleString("en-US", { timeZone: "UTC", hour: 'numeric', minute: 'numeric', hour12: true });
+    let mil2 = date2.toLocaleString("en-US", { timeZone: "UTC", hour: 'numeric', minute: 'numeric', hour12: false });
+    response = `Esfand's local time is ${cst} CST KKona (${mil}) Korea: ${kst} KST (${mil2})`;
 
     client.action(channel, `@${tagged} ${response}`);
   }
